05c62dccf0ccb28ce7a7c1bef2d42e1a7dc1df72
Glitch (hashi0203-iscg-kadai) <none>
2020-04-25 04:29:18 +0000

    💠🐊 Checkpoint ./kadai1/script.js:15135430/7562

--- kadai1/script.js
+++ kadai1/script.js
@@ -80,23 +80,6 @@ function draw_bezier() {
     legacygl.color(0.5, 0.5, 0.5);
     drawutil.xygrid(100);
   
-    // var tmp_num_p = Number(document.getElementById("input_b_numcontrolpoints").value);
-    // if (tmp_num_p < 3) {
-    //   tmp_num_p = 3;
-    //   document.getElementById("input_b_numcontrolpoints").value = 3;
-    // } else if (tmp_num_p > 10) {
-    //   tmp_num_p = 10;
-    //   document.getElementById("input_b_numcontrolpoints").value = 10;
-    // } else {
-    //   tmp_num_p = Math.round(tmp_num_p);
-    //   document.getElementById("input_b_numcontrolpoints").value = tmp_num_p;
-    // }
-    // while (num_p < tmp_num_p) {
-    //   p[num_p] = [1.2+0.1*(num_p-2),0.5+(-1)**(num_p-1)*(0.8-0.1*(num_p-2)),0];
-    //   num_p++;
-    // }
-    // num_p = tmp_num_p;
-  
     var num_p = Number(document.getElementById("input_b_numcontrolpoints").value);
     if (num_p < 3) {
       num_p = 3;
@@ -417,14 +400,6 @@ function draw_3dbezier(){
     legacygl.begin(legacygl.QUADS);
     legacygl.color(0.6, 0, 0.8);
     var step = 1/numsteps;
-    // for (var i = 0; i < numsteps; i++) {
-    //   for (var j = 0; j < numsteps; j++) {
-    //       legacygl.vertex3(eval_3dbezier(p, i*step, j*step));
-    //       legacygl.vertex3(eval_3dbezier(p, i*step, (j+1)*step));
-    //       legacygl.vertex3(eval_3dbezier(p, (i+1)*step, (j+1)*step));
-    //       legacygl.vertex3(eval_3dbezier(p, (i+1)*step, j*step));
-    //   }
-    // }
     var points = Array((numsteps+1)**2);
     for (var i = 0; i <= numsteps; i++) {
         for (var j = 0; j <= numsteps; j++) {
@@ -617,6 +592,7 @@ function settings() {
 }
 
 function cameraview() {
+  init();
   if (document.getElementById("input_bezier").checked) {
     camera.eye = [0, 0, 7];
   } else if (document.getElementById("input_catmull").checked) {
@@ -730,117 +706,5 @@ function init() {
     // init OpenGL settings
     gl.viewport(0, 0, canvas.width, canvas.height);
     gl.clearColor(1, 1, 1, 1);
-  // OpenGL context
-    // canvas = document.getElementById("canvas");
-    // gl = canvas.getContext("experimental-webgl");
-    // if (!gl)
-    //     alert("Could not initialise WebGL, sorry :-(");
-    // var vertex_shader_src = "\
-    //     attribute vec3 a_vertex;\
-    //     attribute vec3 a_color;\
-    //     varying vec3 v_color;\
-    //     uniform mat4 u_modelview;\
-    //     uniform mat4 u_projection;\
-    //     void main(void) {\
-    //         gl_Position = u_projection * u_modelview * vec4(a_vertex, 1.0);\
-    //         v_color = a_color;\
-    //         gl_PointSize = 5.0;\
-    //     }\
-    //     ";
-    // var fragment_shader_src = "\
-    //     precision mediump float;\
-    //     varying vec3 v_color;\
-    //     void main(void) {\
-    //         gl_FragColor = vec4(v_color, 1.0);\
-    //     }\
-    //     ";
-    // legacygl = get_legacygl(gl, vertex_shader_src, fragment_shader_src);
-    // legacygl.add_uniform("modelview", "Matrix4f");
-    // legacygl.add_uniform("projection", "Matrix4f");
-    // legacygl.add_vertex_attribute("color", 3);
-    // drawutil = get_drawutil(gl, legacygl);
-    // camera = get_camera(canvas.width);
-    // camera.eye = [20, 30, 30];
-    // objects = [
-    //     {position: [-2, 3, -4], scale: 2},
-    //     {position: [-3, 2, 5], scale: 4},
-    //     {position: [4, 3, -7], scale: 6}
-    // ];
-    // // event handlers
-    // canvas.onmousedown = function(evt) {
-    //     var mouse_win = this.get_mousepos(evt);
-    //     if (evt.altKey) {
-    //         camera.start_moving(mouse_win, evt.shiftKey ? "zoom" : evt.ctrlKey ? "pan" : "rotate");
-    //         return;
-    //     }
-    //     // pick nearest object
-    //     selected_index = -1;
-    //     var candidates = [];
-    //     var viewport = [0, 0, canvas.width, canvas.height];
-    //     for (var i = 0; i < objects.length; ++i) {
-    //         var object_win = glu.project(objects[i].position, 
-    //                                      legacygl.uniforms.modelview.value,
-    //                                      legacygl.uniforms.projection.value,
-    //                                      viewport);
-    //         if (vec3.dist(mouse_win, object_win) < 20)
-    //             candidates.push({ dist: vec3.dist(camera.eye, objects[i].position), index:i });
-    //     }
-    //     if (candidates.length > 0) {
-    //         candidates.sort(function(a, b){return a.dist - b.dist});
-    //         selected_index = candidates[0].index;
-    //         plane_origin = objects[selected_index].position;
-    //         plane_normal = vec3.normalize_ip(camera.eye_to_center());
-    //         is_translating = true;
-    //     }
-    //     document.getElementById("input_scaling").value =
-    //         selected_index == -1 ? null : objects[selected_index].scale;
-    //     draw();
-    // };
-    // canvas.onmousemove = function(evt) {
-    //     var mouse_win = this.get_mousepos(evt);
-    //     if (camera.is_moving()) {
-    //         camera.move(mouse_win);
-    //         draw();
-    //         return;
-    //     }
-    //     if (is_translating) {
-    //         var viewport = [0, 0, canvas.width, canvas.height];
-    //         mouse_win.push(1);
-    //         var mouse_obj = glu.unproject(mouse_win, 
-    //                                       legacygl.uniforms.modelview.value,
-    //                                       legacygl.uniforms.projection.value,
-    //                                       viewport);
-    //         var eye_to_mouse = vec3.sub([], mouse_obj, camera.eye);
-    //         var eye_to_origin = vec3.sub([], plane_origin, camera.eye);
-    //         var s1 = vec3.dot(eye_to_mouse, plane_normal);
-    //         var s2 = vec3.dot(eye_to_origin, plane_normal);
-    //         var eye_to_intersection = vec3.scale([], eye_to_mouse, s2 / s1);
-    //         objects[selected_index].position = vec3.add([], camera.eye, eye_to_intersection);
-    //         draw();
-    //     }
-    // }
-    // document.onmouseup = function (evt) {
-    //     if (camera.is_moving()) {
-    //         camera.finish_moving();
-    //         return;
-    //     }
-    //     is_translating = false;
-    // };
-    // document.getElementById("input_scaling").onchange = function(evt) {
-    //     if (selected_index == -1)
-    //         return;
-    //     objects[selected_index].scale = evt.target.value;
-    //     draw();
-    // };
-    // document.getElementById("button_add").onclick = function(evt) {
-    //     objects.push({position: [0, 0, 0], scale: 1});
-    //     draw();
-    // };
-    // // init OpenGL settings
-    // gl.viewport(0, 0, canvas.width, canvas.height);
-    // gl.enable(gl.DEPTH_TEST);
-    // gl.enable(gl.POLYGON_OFFSET_FILL);
-    // gl.polygonOffset(1, 1);
-    // gl.clearColor(1, 1, 1, 1);
 };
 
