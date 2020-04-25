6131be1bee0f99bf3935217c9bcc294b23e00b7c
Glitch (hashi0203-iscg-kadai) <none>
2020-04-25 05:29:25 +0000

    💴🐯 Checkpoint ./kadai1/script.js:15135430/212

--- kadai1/script.js
+++ kadai1/script.js
@@ -441,13 +441,17 @@ function draw_3dbezier(){
 };
 
 function perimeter(l,p,t) {
-  if (l == "c0"){
+  var tmp = Array(10);
+  for (var i = 0; i < 10; i++) {
+    tmp[i] = 1;
+  }
+  if (l == 0){ // c0
     return eval_quadratic_bezier([p[0],p[3],p[1],p[2]], t, 4, 0, tmp);
-  } else if (l == "d0"){
+  } else if (l == 1){ // d1
     return eval_quadratic_bezier([p[3],p[6],p[4],p[5]], t, 4, 0, tmp);
-  } else if (l == "c1"){
+  } else if (l == 2){ // c1
     return eval_quadratic_bezier([p[9],p[6],p[8],p[7]], t, 4, 0, tmp);
-  } else if (l == "d1"){
+  } else if (l == 3){ // d0
     return eval_quadratic_bezier([p[0],p[9],p[11],p[10]], t, 4, 0, tmp);
   }
 };
@@ -481,10 +485,10 @@ function draw_3dcoons(){
     var step = 1/numsteps;
     var points = Array((numsteps+1)**2);
   
-    var tmp = Array(10);
-    for (var i = 0; i < 10; i++) {
-      tmp[i] = 1;
-    }
+    // var tmp = Array(10);
+    // for (var i = 0; i < 10; i++) {
+    //   tmp[i] = 1;
+    // }
     
 //     for (var j = 0; j < 4; j++) {
 //       legacygl.begin(gl.LINE_STRIP);
@@ -511,7 +515,7 @@ function draw_3dcoons(){
       for (var i = 0; i <= numsteps; ++i) {
           var t = i / numsteps;
           // eval_quadratic_bezier([p[3*j],p[(3*j+3)%12],p[3*j+1],p[3*j+2]], t, 4, 0, tmp);
-          perimeter("c"+j,p,t);
+          perimeter(j,p,t);
       }
       legacygl.end();
       // draw sample points
@@ -519,7 +523,7 @@ function draw_3dcoons(){
           legacygl.begin(gl.POINTS);
           for (var i = 0; i <= numsteps; ++i) {
               var t = i / numsteps;
-              perimeter("c"+j,p,t);
+              perimeter(j,p,t);
               // eval_quadratic_bezier([p[3*j],p[(3*j+3)%12],p[3*j+1],p[3*j+2]], t, 4, 0, tmp);
           }
           legacygl.end();
